e2617d9f52cf9ff6ba8028dd1eaa3b3127c4a8cc
Marcia Lima <marcia.lima@pagar.me>
2020-02-06 12:01:45 -0300

    Assigning an Elastic IP

--- aws-overview/main.tf
+++ aws-overview/main.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
