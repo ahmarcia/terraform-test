df1be00969510a8af12358c4e69cb064f090d8b4
Marcia Lima <marcia.lima@pagar.me>
2020-02-06 12:15:43 -0300

    Implicit and Explicit Dependencies

--- aws-overview/main.tf
+++ aws-overview/main.tf
@@ -3,12 +3,19 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "aws-overview-guide"
+  acl    = "private"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+
+  depends_on = [aws_s3_bucket.example]
 }
 
 resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+  vpc = true
+  instance = aws_instance.example.id
 }
